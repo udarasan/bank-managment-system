ec1beda8a4e9d4bd7a223f3ff033f4b09d66a461
udarasan <udarassanjeewa@gmail.com>
2021-08-21 20:21:31 +0530

    dashboard Controller

---
+++ src/main/java/com/epic/bankingmanagment/controller/DashBoardController.java
@@ -0,0 +1,45 @@
+package com.epic.bankingmanagment.controller;
+
+import com.epic.bankingmanagment.service.AccountService;
+import com.epic.bankingmanagment.service.CustomerService;
+import com.epic.bankingmanagment.service.UserService;
+import com.epic.bankingmanagment.util.StandardResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/v1/dashboard")
+@CrossOrigin
+public class DashBoardController {
+    @Autowired
+    private AccountService accountService;
+
+    @Autowired
+    private CustomerService customerService;
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping(path = "/customerCount",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity getCustomerCount() {
+        int customerCount = customerService.getAvailableCustomerCount();
+        return new ResponseEntity(new StandardResponse("200", "Done", customerCount), HttpStatus.OK);
+    }
+    @GetMapping(path = "/accountCount",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity getAccountCount() {
+        int accountCount = accountService.getAvailableAccountCount();
+        return new ResponseEntity(new StandardResponse("200", "Done", accountCount), HttpStatus.OK);
+    }
+    @GetMapping(path = "/employeeCount",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity getEmployeeCount() {
+        int employeeCount = customerService.getAvailableCustomerCount();
+        return new ResponseEntity(new StandardResponse("200", "Done", employeeCount), HttpStatus.OK);
+    }
+}
